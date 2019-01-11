a7803988ce52b5e35a4100582d7c10537dab078c
Vince Andrew Pimentel <contact@vincep.me>
2019-01-11 22:08:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,4 +35,13 @@ class ApplicationController < Sinatra::Base
 
     erb :edit
   end
+
+  patch "/articles/:id" do
+    article = Article.find(params[:id])
+    article.title = params[:title]
+    article.content = params[:content]
+    article.save
+
+    erb :show
+  end
 end
