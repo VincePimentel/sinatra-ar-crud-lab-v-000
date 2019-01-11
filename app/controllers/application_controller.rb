46a7ac6f15cf6f1aed3951e191e280dc572a14fb
Vince Andrew Pimentel <contact@vincep.me>
2019-01-11 22:30:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
   get "/articles/:id/edit" do
     @article = Article.find(params[:id])
 
-    redirect to "/articles/#{params[:id]}"
+    erb :edit
   end
 
   patch "/articles/:id" do
